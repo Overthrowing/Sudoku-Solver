b762e3335e8ea2f6af4b9955d99f0f5a449b463b
Siddharth R <Siddharth.Radh@gmail.com>
2021-06-16 18:28:40 -0500

    Added small QOL changes

--- GUI.py
+++ GUI.py
@@ -1,8 +1,6 @@
-import time
-
 import PySimpleGUI as sg
-
 from main import *
+import time
 
 BOX_SIZE = 47
 layout = [
@@ -20,10 +18,12 @@ for row in range(9):
                     (row * BOX_SIZE + 18, col * BOX_SIZE + 17), text_location="center")
 
 
-def solve(board):
+def solve(board, t=0):
     if solved(board):
         while True:
-            time.sleep(10)
+            event, values = window.read()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                break
         return True
     else:
         row, col = find_empty(board)
@@ -31,19 +31,19 @@ def solve(board):
     for i in range(1, 10):
         if valid(board, i, row, col):
             board[row][col] = i
-            draw(board, row, col)
+            draw(board, row, col, t)
             window.finalize()
-            if solve(board):
+            if solve(board, t):
                 return True
 
             board[row][col] = 0
-            draw(board, row, col)
+            draw(board, row, col, t)
             window.finalize()
 
     return False
 
 
-def draw(board, r, c):
+def draw(board, r, c, t):
     g.erase()
     for row in range(9):
         for col in range(9):
@@ -60,7 +60,7 @@ def draw(board, r, c):
 
             g.draw_text('{}'.format(board[row][col]),
                         (col * BOX_SIZE + 10, row * BOX_SIZE + 8), font=("courier", 17), text_location="center")
-    time.sleep(0.3)
+    time.sleep(t)
 
-
-solve(board)
+if __name__ == '__main__':
+    solve(board, 0.3)
